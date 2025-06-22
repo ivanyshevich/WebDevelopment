d65706f84ff71d12261433c4a44536adde2ca929
ivanyshevich <KryvosheyaIvan@gmail.com>
2025-06-22 18:44:12 +0300

    sd

--- WebDev_lab11/backend_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "_db.php";
-
-$stmt = $db->prepare("DELETE FROM reservations WHERE id = :id");
-$stmt->execute([':id' => $_POST['id']]);
-
-$response = ['result'=>'OK','message'=>'Deleted'];
-header('Content-Type: application/json');
-echo json_encode($response);
